95213be3b62b7729577286846cbf1291862e73f5
Joshua Olivar <joshuaolivar@Joshuas-MacBook-Air.local>
2019-11-01 06:49:09 -0700

    final commit

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+	{
+		name: "Josh",
+		photo: "josh.jpg",
+        scores:[3,
+                5,
+                4,
+                2,
+                1,
+                2,
+                5,
+                1,
+                2,
+                4]
+	},
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+var express = require("express");
+
+// Set up Express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Set up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+    console.log("Listening on PORT: " + PORT);
+})
